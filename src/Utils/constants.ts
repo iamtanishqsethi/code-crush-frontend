00f16f7359eb6e64e58aa95f1c28bead1ce5d49a
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-05-10 15:57:34 +0530

    fix : backend hosting url fix

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL:string="https://api.render.com/deploy/srv-d0fia83uibrs73envvjg?key=2sax-8Q5sGY"
+export const BASE_URL:string="https://code-crush-backend.onrender.com"
 // export const BASE_URL:string="http://localhost:7777"
