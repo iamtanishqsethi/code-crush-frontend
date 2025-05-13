5c771b37bdff77b4a433ea235cfe9a197c45de06
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-05-13 19:41:42 +0530

    fix: websocket fix

--- src/Utils/socket.ts
+++ src/Utils/socket.ts
@@ -3,14 +3,6 @@ import {BASE_URL} from "@/Utils/constants.ts";
 
 
 export const createSocketConnection=()=>{
-    if(location.hostname === 'localhost'){
-        return io(BASE_URL)
-    }else {
-        return io(BASE_URL,
-            {
-                transports:["websocket"]
-            })
-    }
-
+    return io(BASE_URL)
 
 }
