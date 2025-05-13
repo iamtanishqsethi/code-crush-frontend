48e182c5a76c872563c4fccb2f331593df3ca11a
Tanishq Sethi <tanishqsethi05@gmail.com>
2025-05-13 20:00:07 +0530

    fix: websocket fix

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -1,9 +1,10 @@
 
-export let BASE_URL:string
-
-if(location.hostname === "localhost"){
-    BASE_URL="http://localhost:7777"
-}
-else {
-    BASE_URL="https://code-crush-backend.onrender.com"
-}
+export const BASE_URL:string="https://code-crush-backend.onrender.com"
+// let url:string=
+//
+// if(location.hostname === "localhost"){
+//     BASE_URL="http://localhost:7777"
+// }
+// else {
+//     BASE_URL="https://code-crush-backend.onrender.com"
+// }
